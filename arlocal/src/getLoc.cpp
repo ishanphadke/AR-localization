756c0df94541b581327a0d084757a4bf5e7c3232
ishanphadke <44324805+ishanphadke@users.noreply.github.com>
2019-05-19 22:25:39 -0500

    Update getLoc.cpp

--- arlocal/src/getLoc.cpp
+++ arlocal/src/getLoc.cpp
@@ -4,38 +4,12 @@
 
   getLoc::getLoc(tf::TransformListener &tfL, MoveBaseClient &ac) : _tfL(tfL), _ac(ac) {}
 
-  geometry_msgs::Pose getLoc::receivePose(geometry_msgs::Pose &pose, int markerID)
+  geometry_msgs::Pose getLoc::receivePose(geometry_msgs::Pose &pose, int mID)
   {
-  	geometry_msgs::Pose markerPos= pose;
-
-    while(!_ac.waitForServer(ros::Duration(5.0))){
-      ROS_INFO("Waiting for the move_base action server to come up");
-    }
-
-  	move_base_msgs::MoveBaseGoal goal;
-
-  	goal.target_pose.header.frame_id= "base_link";
-	  goal.target_pose.header.stamp= ros::Time::now();
-
-    double x= markerPos.position.z;
-    double y= markerPos.position.x;
-    double z= markerPos.position.y;
-    double distance= sqrt(pow(x, 2)+pow(y, 2));
-  	goal.target_pose.pose.position.x= distance;
-
-    double angle= atan2(y,x);
-    std::cout << "Angle: "<<angle<<'\n';
-    tf::Quaternion direction;
-    direction.setRPY(0,0,0.8*angle);
-    goal.target_pose.pose.orientation.w= direction.w();
-    goal.target_pose.pose.orientation.x= direction.x();
-    goal.target_pose.pose.orientation.y= direction.y();
-    goal.target_pose.pose.orientation.z= direction.z();
-
-   if(!markerFound)
-    {
-	   
-    }
+    geometry_msgs::Pose markerPos= pose;
+    markerFound= true;
+    markerID= mID;
+    markerPose= pose;
   }
 
   bool getLoc::seenMarker()
